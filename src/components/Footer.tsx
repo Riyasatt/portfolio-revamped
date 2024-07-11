009518b3a9562d1d802bd22b83cd19d37e628e93
Riyasatt <kriyasatali04@gmail.com>
2024-07-11 07:32:01 +0530

    test commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Heart } from "lucide-react"
 const Footer = () => {
   return (
     <div className="w-full bg-gradient flex ">
-        <div className="container mx-auto items-center justify-center  py-4 flex items-center gap-1">
+        <div className="container mx-auto items-center justify-center  py-4 flex  gap-1">
                Made with<Heart /> by Riyasat
         </div>
 
